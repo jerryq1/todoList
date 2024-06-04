486ca0720a40ca8626792f0e91f549ce8b9ebcc1
jerry <xuzhanhong@gialen.com>
2024-06-04 19:04:47 +0800

    更新代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./component/Header";
+import ToDoList from "./component/ToDoList";
+import AddToDo from "./component/AddToDo";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const localStorageTodos = localStorage.getItem('todos')
+    const storedTodos = JSON.parse(localStorage.getItem('todos'));
+    const [todos, setTodos] = useState(localStorageTodos? storedTodos:[]);
 
+
+
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem('todos'));
+        if (storedTodos) setTodos(storedTodos);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+    const addTodo = (title) => {
+        const newTodo = { id: Date.now(), title, completed: false };
+        setTodos([...todos, newTodo]);
+    };
+
+    const toggleComplete = (id) => {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
+
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Header/>
+        <ToDoList todos={todos} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
+        <AddToDo addTodo={addTodo}/>
     </>
   )
 }
